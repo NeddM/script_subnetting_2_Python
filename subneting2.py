76d500fe5afce35b6b44d2910418c8f89c12b69a
Nedd Chairi Muñoz <91211902+illoneddy@users.noreply.github.com>
2022-04-25 13:40:47 +0200

    Add files via upload

---
+++ subneting2.py
@@ -0,0 +1,88 @@
+# INPUTS
+# ip = input('Inserta la IP: ')
+ip = '203.34.134.81'
+mascara = input('Inserta el número de bits de la máscara: ')
+
+# MASCARA SEPARADA
+separada = ip.split('.')
+
+def que_clase(separada):
+    x = int(separada[0])
+    if x >= 0 and x <= 127:
+        return [255, 0, 0, 0]
+        # print('Es clase A')
+    if x >= 128 and x <= 191:
+        return [255, 255, 0, 0]
+        # print('Es clase B')
+    if x >= 192 and x <= 223:
+        return [255, 255, 255, 0]
+        # print('Es clase C')
+    if x >= 224 and x <= 239:
+        return [255, 255, 255, 255]
+        # print('Es clase D')
+    if x >= 240 and x <= 247:
+        print('Es clase E')
+
+
+
+def a_binario_ip(lista):
+    o1, o2, o3 ,o4 = lista
+
+    bino1 = bin(int(o1))
+    bino2 = bin(int(o2))
+    bino3 = bin(int(o3))
+    bino4 = bin(int(o4))
+
+    binaria = [bino1, bino2, bino3, bino4]
+    return binaria
+
+
+def a_binario_mascara(lista):
+    o1, o2, o3 ,o4 = lista
+
+    bino1 = bin(int(o1))
+    bino2 = bin(int(o2))
+    bino3 = bin(int(o3))
+    bino4 = bin(int(o4))
+
+    binaria = [bino1, bino2, bino3, bino4]
+    return binaria
+
+
+
+def comparar(listip, listmascara):
+    oip1 = int(listip[0], 2)
+    oma1 = int(listmascara[0], 2)
+    and1 = oip1 & oma1
+
+    oip2 = int(listip[1], 2)
+    oma2 = int(listmascara[1], 2)
+    and2 = oip2 & oma2
+
+    oip3 = int(listip[2], 2)
+    oma3 = int(listmascara[2], 2)
+    and3 = oip3 & oma3
+
+    oip4 = int(listip[3], 2)
+    oma4 = int(listmascara[3], 2)
+    and4 = oip4 & oma4
+
+
+
+
+    print(bin(and1))
+    print(bin(and2))
+    print(bin(and3))
+    print(bin(and4))
+
+
+hola = comparar(a_binario_ip(separada), a_binario_mascara(que_clase(separada)))
+print(hola)
+
+
+print(a_binario_ip(separada))
+print(a_binario_mascara(que_clase(separada)))
+
+
+
+
